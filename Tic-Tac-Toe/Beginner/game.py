655887afbc91d4dd86bf275676a45b4de27c0591
Sivakamy Lakshminarayanan <silakshm@microsoft.com>
2024-07-26 21:12:58 +0530

    First push

---
+++ Tic-Tac-Toe/Beginner/game.py
@@ -0,0 +1,16 @@
+
+'''
+Code for Tic-Tac-Toe with a 3x3 grid and 2 players alternatively filling Xs and Os.
+To win:
+One player is able to place 3 symbols of the same type either
+ - horizontally
+ - vertically
+ - diagonally
+
+ Stopping criteria:
+ - If and when any player wins 
+ OR
+ - If all 9 spots in the grid are filled, in which it becomes a draw
+'''
+
+board = [[None]*3] * 3
